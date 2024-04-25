f0c11b5c3f1f40ba8c889ec735da2a3a6f81f463
Chhao2002 <chhao2012200@gmail.com>
2024-04-26 01:35:13 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const app = require("./app");
 const config = require("./app/config");
+const MongoDB = require("./app/utils/mongodb.util");
 
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log(`chay ${PORT}.`);
-});
+async function startServer() {
+    try {
+        await MongoDB.connect(config.db.uri);
+        console.log("ket noi du lieu");
+
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+            console.log(`server dang chay tren port ${PORT}`);
+        });
+    } catch (error) {
+        console.log("khong the ket noi toi du lieu", error);
+        process.exit();
+    }
+}
+
+startServer();
